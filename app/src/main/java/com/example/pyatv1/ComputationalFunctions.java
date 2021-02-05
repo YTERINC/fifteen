e9d5912166efd419f04780043ec655203edad031
Yuriy <jt.darman\@gmail.com>
2021-02-05 08:52:28 +0700

    Создана начальная иницилиазация массива со случайными значениями от 0 до 15. Создана начальная инициализация графики. можно перемещать. Теперь варианты всегда решаемы. Добавлена проверка на выйгрыш.

---
+++ app/src/main/java/com/example/pyatv1/ComputationalFunctions.java
@@ -0,0 +1,108 @@
+package com.example.pyatv1;
+
+import java.util.Random;
+
+public class ComputationalFunctions {
+
+    // координаты нулевого значения массива
+    int i0;
+    int j0;
+
+    public void createMixedArray(int[][] arr) {
+        int count = 1; // для заполнения массива по порядку с 1 до 15
+        int tempArr;  // для временного хранения значения с нулем
+        int rand;
+        Random rnd = new Random();
+        // заполняем массив по порядку
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = + count;
+                ++count;
+            }
+        arr[3][3] = 0; // последней клетке присваиваем ноль, т.е. это будет пустая клетка
+        i0 = 3;
+        j0 = 3;
+
+        // размешиваем массив правильными ходами
+         for (int count2 = 0; count2 < 500; count2++) {
+             rand = rnd.nextInt(4); // случайный ход от 0 до 3
+              // rand=0 - перемещение пустой клетке (0) влево
+              // rand=1 - перемещение пустой клетке (0) вверх
+              // rand=2 - перемещение пустой клетке (0) вправо
+              // rand=3 - перемещение пустой клетке (0) вниз
+             if (i0 == 0 && rand == 1) rand = 3; // т.е. если вверх нельзя переместить мы идем вниз
+             if (i0 == 3 && rand == 3) rand = 1; // т.е. если винз нельзя переместить мы идем вверх
+             if (j0 == 0 && rand == 0) rand = 2; // т.е. если влево нельзя переместить мы идем вправо
+             if (j0 == 3 && rand == 2) rand = 0; // т.е. если вправо нельзя переместить мы идем влево
+
+             switch (rand) {
+                 case 0: {
+                     tempArr = arr[i0][j0];
+                     arr[i0][j0] = arr[i0][j0-1];
+                     arr[i0][j0-1] = tempArr;
+                     j0 = j0-1; break;}
+                 case 1: {
+                     tempArr = arr[i0][j0];
+                     arr[i0][j0] = arr[i0-1][j0];
+                     arr[i0-1][j0] = tempArr;
+                     i0 = i0-1; break;}
+                 case 2: {
+                     tempArr = arr[i0][j0];
+                     arr[i0][j0] = arr[i0][j0+1];
+                     arr[i0][j0+1] = tempArr;
+                     j0 = j0+1; break;}
+                 case 3: {
+                     tempArr = arr[i0][j0];
+                     arr[i0][j0] = arr[i0+1][j0];
+                     arr[i0+1][j0] = tempArr;
+                     i0 = i0+1; break;}
+             }
+         }
+    }
+
+    public int calculateSmallSquareX1(int sX3, int sXd, int distBetweenSquares, int sJ) {
+        return sX3 + distBetweenSquares + (sXd + distBetweenSquares) * sJ;
+    }
+
+    public int calculateSmallSquareX2(int sX3, int sXd, int distBetweenSquares, int sJ) {
+        return sX3 + distBetweenSquares + sXd + (sXd + distBetweenSquares) * sJ;
+    }
+
+    public int calculateSmallSquareY1(int sY3, int sYd, int distBetweenSquares, int sI) {
+        return sY3 + distBetweenSquares + (sYd + distBetweenSquares) * sI;
+    }
+
+    public int calculateSmallSquareY2(int sY3, int sYd, int distBetweenSquares, int sI) {
+        return sY3 + distBetweenSquares + sYd + (sYd + distBetweenSquares) * sI;
+    }
+
+    public int sizeOneSquareX(int sX4, int sX3, int distBetweenSquares) {
+        return (sX4 - sX3 - distBetweenSquares * 5) / 4;
+    }
+
+    public int sizeOneSquareY(int Sy4, int sY3, int distBetweenSquares) {
+        return (Sy4 - sY3 - distBetweenSquares * 5) / 4;
+    }
+
+    public boolean checkWin(int[][] arrWin) {
+        int winCount = 0;
+        int winCheck = 0;
+        boolean winRes = false;
+
+        for (int i1 = 0; i1 < 4; i1++)
+            for (int j1 = 0; j1 < 4; j1++) {
+                winCount++;
+                if (arrWin[i1][j1] == winCount) winCheck++;
+                if (winCheck == 15) winRes = true;
+            }
+        return winRes;
+    }
+
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/pyatv1/MainGameGraphics.java
@@ -0,0 +1,171 @@
+package com.example.pyatv1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.core.content.ContextCompat;
+
+
+public class MainGameGraphics extends View {
+    public Paint mPaint = new Paint();
+    public ComputationalFunctions сompFunct = new ComputationalFunctions();
+    int[][] arr = new int[4][4];
+    int countStart = 0; // счетчик количества вывозов onDraw
+    final int X1 = 50; //отступ рамки, отсюда идет расчет остальных размеров // координата X внешнего квадрата, слева
+    final int Y1 = 200; //отступ рамки, отсюда идет расчет остальных размеров // координата Y внешнего квадрата, слева
+    int x2; //координата X внешнего квадрата, справа
+    int y2; //координата Y внешнего квадрата, справа
+    final int THICKNESS_BIG_FRAME = 10; // толщина рамки большого квадрата
+    // координаты меньшего квадрата для отображения рамки
+    int x3; //координата X внутреннего квадрата, слева
+    int y3; //координата Y внутреннего квадрата, слева
+    int x4; //координата X внутреннего квадрата, справа
+    int y4; //координата Y внутреннего квадрата, справа
+    // координаты квадратиков
+    int xin1 = 0; // координата X маленького квадрата, слева
+    int yin1 = 0; // координата Y маленького квадрата, слева
+    int xin2 = 0; // координата X маленького квадрата, справа
+    int yin2 = 0; // координата Y маленького квадрата, справа
+    final int DIST_BETWEEN_SQUARES = 10; // промежуток между квадратиками
+    // размеры одного квадратика для рассчетов
+    int xD;
+    int yD;
+
+    public MainGameGraphics(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        x2 = width - X1;
+        y2 = x2 - X1 + Y1;
+        // рамка внутреннего большого квадрата
+        x3 = X1 + THICKNESS_BIG_FRAME;
+        y3 = Y1 + THICKNESS_BIG_FRAME;
+        x4 = x2 - THICKNESS_BIG_FRAME;
+        y4 = y2 - THICKNESS_BIG_FRAME;
+
+        // стиль Заливка
+        mPaint.setStyle(Paint.Style.FILL);
+        // закрашиваем холст
+        mPaint.setColor(Color.GREEN);
+
+        canvas.drawPaint(mPaint);
+        // Рисуем внешний прямоугольник
+        mPaint.setColor(Color.BLUE);
+        canvas.drawRect(X1, Y1, x2, y2, mPaint);
+        // Рисуем внутренний прямоугольник
+        mPaint.setColor(Color.GREEN);
+        canvas.drawRect(x3, y3, x4, y4, mPaint);
+
+/////// Инициализация массива
+        if (countStart == 0) {
+            сompFunct.createMixedArray(arr);
+        }
+ ///////////////////////////////////
+
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) {
+                if (arr[i][j] != 0) {
+                    mPaint.setColor(Color.YELLOW);
+                    xD = сompFunct.sizeOneSquareX(x4, x3, DIST_BETWEEN_SQUARES);
+                    yD = сompFunct.sizeOneSquareY(y4, y3, DIST_BETWEEN_SQUARES);
+                    xin1 = сompFunct.calculateSmallSquareX1(x3, xD, DIST_BETWEEN_SQUARES, j);
+                    xin2 = сompFunct.calculateSmallSquareX2(x3, xD, DIST_BETWEEN_SQUARES, j);
+                    yin1 = сompFunct.calculateSmallSquareY1(y3, yD, DIST_BETWEEN_SQUARES, i);
+                    yin2 = сompFunct.calculateSmallSquareY2(y3, yD, DIST_BETWEEN_SQUARES, i);
+                    canvas.drawRect(xin1, yin1, xin2, yin2, mPaint);
+                    mPaint.setColor(Color.RED);
+                    mPaint.setStyle(Paint.Style.FILL);
+                    mPaint.setAntiAlias(true);
+                    mPaint.setTextSize(yD * 2 / 3);
+                    mPaint.setTextAlign(Paint.Align.CENTER);
+                    canvas.drawText(String.valueOf(arr[i][j]), xin2 - (xin2 - xin1) / 2, yin2 - (yin2 - yin1) / 2 + yD / 4, mPaint);
+                }
+            }
+
+            // индикаторы
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(48);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText(String.valueOf(сompFunct.i0), 30, height - 32, mPaint);
+        canvas.drawText(String.valueOf(сompFunct.j0), 30, height - 96, mPaint);
+        canvas.drawText(String.valueOf(сompFunct.checkWin(arr)), 30, height - 150, mPaint);
+        /////////////////
+        countStart = +1;
+
+    }
+
+
+    public boolean onTouchEvent(MotionEvent event) {
+        // координаты Touch-события
+        float evX = event.getX();
+        float evY = event.getY();
+
+        int tempArr;  // для временного хранения значения с нулем
+
+        switch (event.getAction()) {
+            // касание началось
+            case MotionEvent.ACTION_DOWN:
+                // если касание было начато в пределах квадрата
+                if (evX >= сompFunct.calculateSmallSquareX1(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0-1) &&
+                        evX <= сompFunct.calculateSmallSquareX2(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0-1) &&
+                        evY >= сompFunct.calculateSmallSquareY1(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0) &&
+                        evY <= сompFunct.calculateSmallSquareY2(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0) &&
+                        сompFunct.j0 > 0) {
+                    tempArr = arr[сompFunct.i0][сompFunct.j0];
+                    arr[сompFunct.i0][сompFunct.j0] = arr[сompFunct.i0][сompFunct.j0-1];
+                    arr[сompFunct.i0][сompFunct.j0-1] = tempArr;
+                    сompFunct.j0 = сompFunct.j0-1;
+                   invalidate();
+                }
+                if (evX >= сompFunct.calculateSmallSquareX1(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0+1) &&
+                        evX <= сompFunct.calculateSmallSquareX2(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0+1) &&
+                        evY >= сompFunct.calculateSmallSquareY1(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0) &&
+                        evY <= сompFunct.calculateSmallSquareY2(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0) &&
+                        сompFunct.j0 < 3) {
+                    tempArr = arr[сompFunct.i0][сompFunct.j0];
+                    arr[сompFunct.i0][сompFunct.j0] = arr[сompFunct.i0][сompFunct.j0+1];
+                    arr[сompFunct.i0][сompFunct.j0+1] = tempArr;
+                    сompFunct.j0 = сompFunct.j0+1;
+                    invalidate();
+                }
+                if (evX >= сompFunct.calculateSmallSquareX1(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0) &&
+                        evX <= сompFunct.calculateSmallSquareX2(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0) &&
+                        evY >= сompFunct.calculateSmallSquareY1(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0-1) &&
+                        evY <= сompFunct.calculateSmallSquareY2(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0-1) &&
+                        сompFunct.i0 > 0) {
+                    tempArr = arr[сompFunct.i0][сompFunct.j0];
+                    arr[сompFunct.i0][сompFunct.j0] = arr[сompFunct.i0-1][сompFunct.j0];
+                    arr[сompFunct.i0-1][сompFunct.j0] = tempArr;
+                    сompFunct.i0 = сompFunct.i0-1;
+                    invalidate();
+                }
+                if (evX >= сompFunct.calculateSmallSquareX1(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0) &&
+                        evX <= сompFunct.calculateSmallSquareX2(x3, xD, DIST_BETWEEN_SQUARES, сompFunct.j0) &&
+                        evY >= сompFunct.calculateSmallSquareY1(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0+1) &&
+                        evY <= сompFunct.calculateSmallSquareY2(y3, yD, DIST_BETWEEN_SQUARES, сompFunct.i0+1) &&
+                        сompFunct.i0 < 3) {
+                    tempArr = arr[сompFunct.i0][сompFunct.j0];
+                    arr[сompFunct.i0][сompFunct.j0] = arr[сompFunct.i0+1][сompFunct.j0];
+                    arr[сompFunct.i0+1][сompFunct.j0] = tempArr;
+                    сompFunct.i0 = сompFunct.i0+1;
+                    invalidate();
+                }
+                break;
+        }
+        return true;
+    }
+
+}
